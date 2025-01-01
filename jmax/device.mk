4e3fef9419c07ce7edc6f3ded4c760f122baa6fe
remittor <remittor@gmail.com>
2025-01-01 12:10:37 +0300

    config: Fix partitions parameters

--- jmax/device.mk
+++ jmax/device.mk
@@ -4,24 +4,22 @@ LOCAL_PATH := device/unihertz/jmax
 # Dynamic
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# A/B
+# A/B partitions
 ENABLE_VIRTUAL_AB := true
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS += \
-    boot \
     dtbo \
-    product \
-    odm \
-    odm_dlkm \
     system \
+    vendor \
+    product \
     system_ext \
-    system_dlkm \
-    vbmeta \
-    vbmeta_system \
-    vbmeta_vendor \
+    boot \
     vendor \
     vendor_boot \
     vendor_dlkm \
+    vbmeta \
+    vbmeta_vendor \
+    vbmeta_system \
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -32,11 +30,12 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     bootctrl \
+    bootctrl.recovery \
     android.hardware.boot@1.2-service \
     android.hardware.boot@1.2-mtkimpl \
     android.hardware.boot@1.2-mtkimpl.recovery \
 
-PRODUCT_PACKAGES := \
+PRODUCT_PACKAGES += \
     libgptutils \
     libz \
     libcutils \
@@ -47,7 +46,7 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload \
-    checkpoint_gc \
+    update_engine_client \
 
 # Health
 PRODUCT_PACKAGES += \
