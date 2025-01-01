ed2b90f431c09dc63dacc63000840f1c6ee683fd
remittor <remittor@gmail.com>
2025-01-01 14:17:24 +0300

    init: Adapt tee meta mirrors to current device (remove persist)
    Reason: persist not presented

--- jmax/recovery/root/vendor/bin/init_trustkernel_twrp.sh
+++ jmax/recovery/root/vendor/bin/init_trustkernel_twrp.sh
@@ -1,7 +1,4 @@
 #!/sbin/sh
 
-rm -rf  /mnt/vendor/persist/t6_twrp/*
-cp -rfp /mnt/vendor/persist/t6/* /mnt/vendor/persist/t6_twrp
-
 rm -rf  /mnt/vendor/protect_f/tee_twrp/*
 cp -rfp /mnt/vendor/protect_f/tee/* /mnt/vendor/protect_f/tee_twrp
